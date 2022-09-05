996f33a4d71da689e3566e75d7db6d6ad2d3f3a8
ccj777 <002ntou@gmail.com>
2022-09-05 13:45:49 +0800

    feat:分類顯示功能

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const Expenses = require('../../models/expense')
 
+
+// 導向首頁
 router.get('/', (req, res) => {
   // const userId = req.user._id
   //{ userId }
@@ -13,9 +15,25 @@ router.get('/', (req, res) => {
       for (let i = 0; i < expenses.length; i++) {
         totalAmount += expenses[i].amount
       }
-      res.render('index', { expenses,totalAmount })
+      res.render('index', { expenses, totalAmount })
     })
-    .catch(error => console.error(error))
+
+})
+
+// POST select表單
+router.post('/category', (req, res) => {
+  console.log(req.body)
+  const category = req.body.category
+  Expenses.find({ categoryId: category })
+    .lean()
+    .then(expenses => {
+      let totalAmount = 0
+      for (let i = 0; i < expenses.length; i++) {
+        totalAmount += expenses[i].amount
+      }
+      res.render('index', { expenses, totalAmount })
+    }).catch(error => console.error(error))
+
 })
 
 // 匯出路由模組
