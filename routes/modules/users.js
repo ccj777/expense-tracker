caf6a4e1b1941c20a938d92eedf2f2ef94f2c831
ccj777 <002ntou@gmail.com>
2022-09-05 14:17:17 +0800

    feat: add register page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const router = express.Router()
 
+// 導向login頁面
 router.get('/login', (req, res) => {
   res.render('login')
 })
+
+// 導向register頁面
+router.get('/register',(req, res) => {
+  res.render('register')
+})
+
+// register 表單送出
+router.post('/register',(req, res) => {
+
+})
+
 
 module.exports = router
